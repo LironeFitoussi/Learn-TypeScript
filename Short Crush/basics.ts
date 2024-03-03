d9d238b3538e15fdf3de223fe2dbcba3ca0588cf
LironeFitoussi <95374011+LILo2610@users.noreply.github.com>
2024-03-03 02:44:31 +0200

    added basics

--- Short Crush/basics.ts
+++ Short Crush/basics.ts
@@ -1,5 +1,20 @@
 // Primitives: number, string boolean
-
 // More complex types: arrays, objects
-
 // function types, parameters
+
+// Primitives
+let age: number;
+age = 12;
+// age = "string" >> Error
+
+let userName: string;
+userName = "John";
+// userName = 12 >> Error
+
+let isInstructor: boolean;
+isInstructor = true;
+// isInstructor = "NotInstructor" >> Error
+
+let hobbies: null;
+// hobbies = "not" >> Error
+
