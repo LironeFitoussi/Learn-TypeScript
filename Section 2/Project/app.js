435d354431858a2edca668dcc0d7bec3e3d9229e
LironeFitoussi <95374011+LILo2610@users.noreply.github.com>
2024-03-04 18:17:20 +0200

    Lesson 20 Done

--- Section 2/Project/app.js
+++ Section 2/Project/app.js
@@ -1,6 +1,46 @@
+// const person: {
+//   name: string;
+//   age: number;
+//   occupation: string;
+// } = {
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string]
+// } = {
+//   name: "John",
+//   age: 30,
+//   hobbies: ['Sports', 'Cooking'],
+//   role: [2, "author"]
+// };
+// const ADMIN = 0;
+// const READ_ONLY= 1
+// const AUTHOR = 2;
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 2] = "AUTHOR";
+})(Role || (Role = {}));
+;
 var person = {
     name: "John",
     age: 30,
-    occupation: "Developer",
+    hobbies: ['Sports', 'Cooking'],
+    role: Role.ADMIN,
 };
-console.log(person.nickname);
+// person.role.push('admin')
+// person.role[1] = 10;
+// person.role = [0, 'admin', 'user']
+var favoriteActivities;
+favoriteActivities = ['Sports'];
+console.log(person);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
+    //  console.log(hobby.map());
+}
+if (person.role === Role.AUTHOR) {
+    console.log('is admin');
+}
--- Section 2/Project/app.ts
+++ Section 2/Project/app.ts
@@ -3,10 +3,44 @@
 //   age: number;
 //   occupation: string;
 // } = {
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string]
+// } = {
+//   name: "John",
+//   age: 30,
+//   hobbies: ['Sports', 'Cooking'],
+//   role: [2, "author"]
+// };
+
+// const ADMIN = 0;
+// const READ_ONLY= 1
+// const AUTHOR = 2;
+
+enum Role {ADMIN = 'ADMIN', READ_ONLY = 100, AUTHOR = 'AUTHOR'};
 const person = {
   name: "John",
   age: 30,
-  occupation: "Developer",
+  hobbies: ['Sports', 'Cooking'],
+  role: Role.ADMIN,
 };
 
-console.log(person.);
+// person.role.push('admin')
+// person.role[1] = 10;
+
+// person.role = [0, 'admin', 'user']
+
+let favoriteActivities: string[];
+favoriteActivities = ['Sports'] 
+console.log(person);
+
+for (let hobby of person.hobbies) {
+ console.log(hobby.toUpperCase());
+//  console.log(hobby.map());
+}
+
+if (person.role === Role.AUTHOR) {
+  console.log('is admin');
+}
