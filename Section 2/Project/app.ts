8139d0f8ed3b3bed3a54d74c32ac07e1d5ab0e56
LironeFitoussi <95374011+LILo2610@users.noreply.github.com>
2024-03-05 20:31:38 +0200

    lesson 24

--- Section 2/Project/app.ts
+++ Section 2/Project/app.ts
@@ -1,47 +1,28 @@
-// const person: {
-//   name: string;
-//   age: number;
-//   occupation: string;
-// } = {
-// const person: {
-//   name: string;
-//   age: number;
-//   hobbies: string[];
-//   role: [number, string]
-// } = {
-//   name: "John",
-//   age: 30,
-//   hobbies: ['Sports', 'Cooking'],
-//   role: [2, "author"]
-// };
+function combine(
+    input1: number | string, 
+    input2: number | string , 
+    resultConversion: 'as-number' | 'as-text'
+) {
+    let result;
+    if (typeof input1 === "number" && typeof input2 === "number" || resultConversion === 'as-number') { 
+        result = +input1 + +input2;
+    } else {
+        result = input1.toString() + input2.toString();
+    }
+    // if (resultConversion === 'as-number') {
+    //     return +result
+    // } else {
+    //     return result.toString();
 
-// const ADMIN = 0;
-// const READ_ONLY= 1
-// const AUTHOR = 2;
-
-enum Role {ADMIN = 'ADMIN', READ_ONLY = 100, AUTHOR = 'AUTHOR'};
-const person = {
-  name: "John",
-  age: 30,
-  hobbies: ['Sports', 'Cooking'],
-  role: Role.ADMIN,
-};
-
-// person.role.push('admin')
-// person.role[1] = 10;
-
-// person.role = [0, 'admin', 'user']
-
-let favoriteActivities: string[];
-favoriteActivities = ['Sports'] 
-
-console.log(person.name);
-
-for (let hobby of person.hobbies) {
- console.log(hobby.toUpperCase());
-//  console.log(hobby.map()); // !!! ERROR !!!
+    // }
+    return result;
 }
 
-if (person.role === Role.AUTHOR) {
-  console.log('is admin');
-}
+const combinedAges = combine(30, 26, "as-number");
+console.log(combinedAges);
+
+const combinedStringAges = combine('30', '26', "as-number");
+console.log(combinedStringAges);
+
+const combinedNames = combine('john', 'John', "as-text");
+console.log(combinedNames);
