d4d2a1c1ebe35c9ff4bfeae1373e80fd8a5d7553
LironeFitoussi <95374011+LILo2610@users.noreply.github.com>
2024-03-07 17:51:19 +0200

    62 done

--- Section 5/src/app.ts
+++ Section 5/src/app.ts
@@ -1,5 +1,7 @@
 class Department {
-    name: string;
+    public name: string;
+    private employees: string[] = [];
+
     constructor(n: string) {
         this.name = n;
     }
@@ -7,13 +9,29 @@ class Department {
     describe (this: Department) {
         console.log('Department: ' + this.name);
     }
+
+    addEmployee (employee: string) {
+        // Validation   
+        this.employees.push(employee);
+    }
+
+    printEmployeeInformation() {
+        console.log(this.employees.length);
+        console.log(this.employees);
+    }
 }
 
 const accounting = new Department("Accounting");
 
-// console.log(accounting);
-
 accounting.describe();
 
+accounting.addEmployee("Max");
+accounting.addEmployee("Manu");
+
+// accounting.employees[2] = 'Anna';
+
+accounting.printEmployeeInformation();
+
+// console.log(accounting);
 const accountingCopy = { name: 'DUMMY', describe: accounting.describe };
-accountingCopy.describe(); 
+// accountingCopy.describe(); 
