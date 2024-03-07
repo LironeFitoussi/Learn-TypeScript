30a437b802ccde68074c627b5d4fd94486e07966
LironeFitoussi <95374011+LILo2610@users.noreply.github.com>
2024-03-07 07:37:25 +0200

    Section 4 Done

--- Section 3/src/app.ts
+++ Section 3/src/app.ts
@@ -21,17 +21,47 @@
 
 // console.log(result);
 
-const add = (a: number, b: number = 1) => a + b;
+// const add = (a: number, b: number = 1) => a + b;
 // console.log(add(2, 5));
 
 
-const printOutput: (a: string | number) => void = output => console.log(output);
+// const printOutput: (a: string | number) => void = output => console.log(output);
 
-const button = document.querySelector('button');
+// const button = document.querySelector('button');
 
-if (button) {
-    button.addEventListener('click', event => console.log(event));
+// if (button) {
+//     button.addEventListener('click', event => console.log(event));
+// }
+
+// printOutput(add(5));
+
+const hobbies = ['Sports', 'Cookking']
+// console.log(hobbies[0]);
+const activeHobbies = ['Hiking'];
+
+// activeHobbies.push(hobbies);
+activeHobbies.push(...hobbies);
+
+const person = {
+    firstName: 'Lirone',
+    age: 30,
 }
 
-printOutput(add(5));
+const copiedPerson = { ...person}
+
+const add = (...numbers: number[]) => {
+    return numbers.reduce((curResult, curValue) => {
+        return curResult + curValue;
+    }, 0)
+};
+
+const addedNumbers = add(5, 10, 2, 3.7);
+console.log(addedNumbers);
+
+const [hobby1, hobby2, ...remainingHobbies] = hobbies;
+
+console.log(hobbies, hobby1, hobby2);
+
+const { firstName: userName, age } = person;
+console.log(userName, age, person);
 
