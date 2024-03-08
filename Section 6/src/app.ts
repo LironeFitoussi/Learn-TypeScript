582ca6030c6757aebee5c46f9fc5f8de93345de9
LironeFitoussi <95374011+LILo2610@users.noreply.github.com>
2024-03-08 09:49:15 +0200

    83 Done

--- Section 6/src/app.ts
+++ Section 6/src/app.ts
@@ -1,1 +1,22 @@
-// Code goes here!
+type Admin = {
+    name: string;
+    privilages: string[];
+}
+
+type Employee = {
+    name: string;
+    startDate: Date;
+};
+
+type ElevatedEmployee = Admin & Employee;
+
+const e1: ElevatedEmployee = {
+    name: "John",
+    privilages: ["create-server"],
+    startDate: new Date(),
+}
+
+type Combinable = string | number;
+type Numeric = number | boolean;
+
+type Universal = Combinable & Numeric;
