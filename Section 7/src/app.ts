7dca641503215dd3be0dc901d30fa7ebecd8a3f4
LironeFitoussi <95374011+LILo2610@users.noreply.github.com>
2024-03-09 20:59:12 +0200

    Lesson 96

--- Section 7/src/app.ts
+++ Section 7/src/app.ts
@@ -1,1 +1,19 @@
-// Code goes here!
+// const names: Array<string> = []; //string[]
+// // names[0].split(' ');
+
+// const promise: Promise<string> = new Promise ((resolve, reject) => {
+//     setTimeout(() => {
+//         resolve('This is Done');
+//     }, 2000);
+// });
+
+// promise.then((data) => {
+//     data.split(' ')
+// });
+
+function merge<T, U extends object>(objA: T, objB: U): T & U {
+    return Object.assign({}, objA, objB);
+}
+
+const mergedObj = merge({name: 'Lirone', hobbies: ['Sports']}, { age: 24 });
+console.log(mergedObj); 
