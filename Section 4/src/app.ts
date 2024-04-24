6c22873047308a7e5ebdf8e2fd12e980694fe915
LironeFitoussi <95374011+LILo2610@users.noreply.github.com>
2024-04-24 12:48:32 +0300

    53

--- Section 4/src/app.ts
+++ Section 4/src/app.ts
@@ -17,9 +17,9 @@ age = 29;
 // console.log(add(2, 5));
 // console.log(isOld);
 
-const add = (a: number, b: number = 1) => a + b;
+// const add = (a: number, b: number = 1) => a + b;
 
-console.log(add(2, 5));
+// console.log(add(2, 5));
 
 const printOutput: (a: number | string) => void = output => console.log(output);
 
@@ -29,7 +29,7 @@ if (button) {
   button.addEventListener('click', e => console.log(e));
 }
 
-printOutput(add(5));
+// printOutput(add(5));
 
 const hobbies = ['Sports', 'Cooking'];
 const activeHobbies = ['Hiking'];
@@ -45,3 +45,12 @@ const person = {
 };
 
 const copiedPerson = { ...person };
+
+const add = (...numbers: number[]) => {
+    return numbers.reduce((curResult, curValue) => {
+        return curResult + curValue;
+    }, 0);
+}
+
+const addedNumbers = add(5, 10, 2, 3.7);
+console.log(addedNumbers);
