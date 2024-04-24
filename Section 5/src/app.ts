d2e60ed7917f99ade698f5675854bcf1b4da4dc5
LironeFitoussi <95374011+LILo2610@users.noreply.github.com>
2024-04-24 20:02:53 +0300

    'Update'

--- Section 5/src/app.ts
+++ Section 5/src/app.ts
@@ -1,16 +1,24 @@
-interface Person {
+interface Greetable {
     name: string;
-    age: number;
+
     greet(phrase: string): void;
 }
 
-let user1: Person;
-user1 = {
-    name: 'Lirone',
-    age: 30,
-    greet(phrase: string): void {
+class Person implements Greetable {
+    name: string;
+    age = 30;
+
+    constructor(n: string) {
+        this.name = n;
+    }
+
+    greet(phrase: string) {
         console.log(phrase + ' ' + this.name);
     }
-};
+}
+
+let user1: Greetable;
+user1 = new Person('Max');
 
 user1.greet('Hi there - I am');
+console.log(user1);
