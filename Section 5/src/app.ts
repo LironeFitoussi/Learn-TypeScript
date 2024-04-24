174dd5d1ff2f3a525203b9f1bed9fe95325e9dd7
LironeFitoussi <95374011+LILo2610@users.noreply.github.com>
2024-04-24 14:41:24 +0300

    'Update'

--- Section 5/src/app.ts
+++ Section 5/src/app.ts
@@ -1,19 +1,40 @@
 class Department {
-    name: string;
+    // private id: string;
+    // private name: string;
+    private employees: string[] = [];
 
-    constructor(n: string) {
-        this.name = n;
+    constructor(private id: string ,public name: string) {
+        this.id = id;
+        this.name = name;
     }
 
     describe(this: Department) {
-        console.log("Department: " + this.name);
+        console.log(`Department (${this.id}): ${this.name}`);
+    }
+
+    addEmployee(employee: string) {
+        // validation etc
+        this.employees.push(employee);
+    }
+
+    printEmployeeInformation() {
+        console.log(this.employees.length);
+        console.log(this.employees);
     }
 }
 
-const accounting = new Department('Accounting');
+const accounting = new Department("d1" ,'Accounting');
 console.log(accounting);
+
+accounting.addEmployee('Max');
+accounting.addEmployee('Manu');
+
+// accounting.employees[2] = 'Anna'; // This will not work because employees is private
+
 accounting.describe();
+// accounting.name = 'NEW NAME'; // This will work because name is public by default
+accounting.printEmployeeInformation();
 
-const accountingCopy = { name: "DUMMMY", describe: accounting.describe};
+// const accountingCopy = { name: "DUMMMY", describe: accounting.describe};
 
-accountingCopy.describe(); // Department: undefined
+// accountingCopy.describe(); // Department: undefined
