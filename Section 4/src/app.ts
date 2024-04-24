e2ae80c53042b477cb99f6b10581331ac363ea7f
LironeFitoussi <95374011+LILo2610@users.noreply.github.com>
2024-04-24 12:23:35 +0300

    49

--- Section 4/src/app.ts
+++ Section 4/src/app.ts
@@ -1,1 +1,22 @@
-// Code goes here!
+const userName = 'Max';
+// userName = 'Manu';
+let age = 30;
+
+age = 29;
+
+function add(a: number, b: number) {
+  let result;
+  result = a + b;
+  return result;
+}
+
+// {
+//     let test = 5;
+// }
+
+// if (age > 20) {
+//   let isOld = true;
+// }
+
+// console.log(add(2, 5));
+// console.log(isOld);
