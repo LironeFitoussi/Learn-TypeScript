a6e212593a1adcb14b63dd9bddcf42505bbad11b
LironeFitoussi <95374011+LILo2610@users.noreply.github.com>
2024-04-24 12:36:50 +0300

    50

--- Section 4/src/app.ts
+++ Section 4/src/app.ts
@@ -4,14 +4,10 @@ let age = 30;
 
 age = 29;
 
-function add(a: number, b: number) {
-  let result;
-  result = a + b;
-  return result;
-}
-
-// {
-//     let test = 5;
+// function add(a: number, b: number) {
+//   let result;
+//   result = a + b;
+//   return result;
 // }
 
 // if (age > 20) {
@@ -20,3 +16,17 @@ function add(a: number, b: number) {
 
 // console.log(add(2, 5));
 // console.log(isOld);
+
+const add = (a: number, b: number = 1) => a + b;
+
+console.log(add(2, 5));
+
+const printOutput: (a: number | string) => void = output => console.log(output);
+
+const button = document.querySelector('button');
+
+if (button) {
+  button.addEventListener('click', e => console.log(e));
+}
+
+printOutput(add(5));
