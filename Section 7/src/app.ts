e537c2eb94a67dce3b907f407d344291685c35cb
LironeFitoussi <95374011+LILo2610@users.noreply.github.com>
2024-04-25 13:17:45 +0300

    'Progress-Lesson'

--- Section 7/src/app.ts
+++ Section 7/src/app.ts
@@ -1,1 +1,14 @@
-// Code goes here!
+//! Generics in TypeScript !//
+const names: Array<string> = ['Alice', 'Bob', 'Charlie']; // string[]
+// names[0].split(' '); // ['A', 'l', 'i', 'c', 'e']
+
+const promise: Promise<string> = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve('This is done!');
+  }, 2000);
+});
+
+promise.then(data => {
+  data.split(' ');   
+});
+
