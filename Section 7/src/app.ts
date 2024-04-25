9e6b523ea7a1bd95752f16f56b413e652f58f560
LironeFitoussi <95374011+LILo2610@users.noreply.github.com>
2024-04-25 14:55:03 +0300

    'Progress-Lesson'

--- Section 7/src/app.ts
+++ Section 7/src/app.ts
@@ -42,3 +42,58 @@ function extractAndConvert<T extends object, U extends keyof T>(obj: T, key: U) 
 }
 
 extractAndConvert({name: 'Alice'}, 'name');
+
+// Generic classes
+class DataStorage<T extends string | number | boolean> {
+    private data: T[] = [];
+
+    addItem(item: T) {
+        this.data.push(item);
+    }
+
+    removeItem(item: T) {
+        if (this.data.indexOf(item) === -1) {
+            return;
+        }
+        this.data.splice(this.data.indexOf(item), 1); // -1 if not found
+    }
+
+    getItems() {
+        return [...this.data];
+    }
+}
+
+const textStorage = new DataStorage<string>();
+textStorage.addItem('Alice');
+textStorage.addItem('Bob');
+textStorage.removeItem('Alice');
+console.log(textStorage.getItems());
+
+const numberStorage = new DataStorage<number>();
+
+// const objStorage = new DataStorage<object>();
+// const aliceObj = {name: 'Alice'};
+// objStorage.addItem(aliceObj);
+// objStorage.addItem({name: 'Bob'});
+// // ...
+// objStorage.removeItem(aliceObj);
+// console.log(objStorage.getItems());
+
+// Generic utility types
+interface CourseGoal {
+    title: string;
+    description: string;
+    completeUntil: Date;
+}
+
+function createCourseGoal(title: string, description: string, date: Date): CourseGoal {
+    let courseGoal: Partial<CourseGoal> = {};
+    courseGoal.title = title;
+    courseGoal.description = description;
+    courseGoal.completeUntil = date;
+    return courseGoal as CourseGoal;
+}
+
+const names: Readonly<string[]> = ['Alice', 'Bob'];
+names.push('Charlie'); // Error!
+names.pop(); // Error!
