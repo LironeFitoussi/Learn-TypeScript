de8e5ec6348e46c3ecebb1f3c6762d6bcdff055c
Lirone Fitoussi <95374011+LironeFitoussi@users.noreply.github.com>
2024-04-26 00:03:03 +0300

    Progress-Lesson

--- Section 8/src/app.ts
+++ Section 8/src/app.ts
@@ -6,12 +6,26 @@ function Logger(logString: string) {
     }
 }
 
+function withTemplate(template: string, hookId: string) {
+    console.log('TEMPLATE FACTORY');
+    return function(constructor: any) {
+        console.log('Rendering template');
+        const hookEl = document.getElementById(hookId);
+        const p = new constructor();
+        if (hookEl) {
+            hookEl.innerHTML = template;
+            hookEl.querySelector('h1')!.textContent = p.name;
+        }
+    }
+}
+
+@Logger('LOGGING - PERSON')
+@withTemplate('<h1>My Person Object</h1>', 'app')
 class Person {
     name = 'Max';
 
     constructor() {
         console.log('Creating person object...');
-        
     }
 }
 
