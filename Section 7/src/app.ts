0790df156bd42529b0779b63a9b56beea08924c1
LironeFitoussi <95374011+LILo2610@users.noreply.github.com>
2024-04-25 13:48:55 +0300

    'Progress-Lesson'

--- Section 7/src/app.ts
+++ Section 7/src/app.ts
@@ -1,14 +1,20 @@
 //! Generics in TypeScript !//
-const names: Array<string> = ['Alice', 'Bob', 'Charlie']; // string[]
+// const names: Array<string> = ['Alice', 'Bob', 'Charlie']; // string[]
 // names[0].split(' '); // ['A', 'l', 'i', 'c', 'e']
 
-const promise: Promise<string> = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve('This is done!');
-  }, 2000);
-});
+// const promise: Promise<string> = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve('This is done!');
+//   }, 2000);
+// });
 
-promise.then(data => {
-  data.split(' ');   
-});
+// promise.then(data => {
+//   data.split(' ');   
+// });
 
+function merge<T extends object, U extends object>(objA: T, objB: U) {
+    return Object.assign(objA, objB);
+}
+
+const mergedObj = merge({name: 'Alice', hobbies: ['Sports']}, {age: 30});
+console.log(mergedObj.hobbies);
