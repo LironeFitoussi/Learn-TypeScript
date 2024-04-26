3f548a801e68ecfd87c3ae6df2bd493c29f33b53
LironeFitoussi <95374011+LILo2610@users.noreply.github.com>
2024-04-26 11:36:47 +0300

    'Progress-Lesson'

--- Section 9/src/app.ts
+++ Section 9/src/app.ts
@@ -1,1 +1,81 @@
-// Code goes here!
+// autobind decorator
+function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+    const originalMethod = descriptor.value;
+    const adjDescriptor: PropertyDescriptor = {
+        configurable: true,
+        get() {
+            const boundFn = originalMethod.bind(this);
+            return boundFn;
+        }
+    };
+    return adjDescriptor;
+}
+
+class ProjectInput {
+    templateElement: HTMLTemplateElement;
+    hostElement: HTMLDivElement;
+    element: HTMLFormElement;
+    titleInputElement: HTMLInputElement;
+    descriptionInputElement: HTMLInputElement;
+    peopleInputElement: HTMLInputElement;
+
+    constructor() {
+        this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
+        this.hostElement = document.getElementById('app')! as HTMLDivElement;
+
+        const importedNode = document.importNode(this.templateElement.content, true);
+        // console.log(importedNode); // #document-fragment
+        
+        this.element = importedNode.firstElementChild as HTMLFormElement;
+        // console.log(this.element); // <form id="user-input">
+
+        this.element.id = 'user-input';
+
+        this.titleInputElement = this.element.querySelector('#title') as HTMLInputElement;
+        // console.log(this.titleInputElement); //? <input type="text" id="title">
+        
+        this.descriptionInputElement = this.element.querySelector('#description') as HTMLInputElement;
+        // console.log(this.descriptionInputElement); //? <input type="text" id="description">
+
+        this.peopleInputElement = this.element.querySelector('#people') as HTMLInputElement;
+        // console.log(this.peopleInputElement); //? <input type="number" id="people">
+
+        this.configure();
+        this.attach();
+    }
+
+    private gatherUserInput(): [string, string, number] | void {
+        const enteredTitle = this.titleInputElement.value;
+        const enteredDescription = this.descriptionInputElement.value;
+        const enteredPeople = this.peopleInputElement.value;
+
+        if (enteredTitle.trim().length === 0 || enteredDescription.trim().length === 0 || enteredPeople.trim().length === 0) {
+            alert('Invalid input, please try again!');
+            return;
+        } else {
+            return [enteredTitle, enteredDescription, +enteredPeople];
+        }
+    }
+
+    @autobind
+    private submitHandler(event: Event) {
+        event.preventDefault();
+        // console.log(this.titleInputElement.value); //? title input value
+
+        const userInput = this.gatherUserInput();
+        if (Array.isArray(userInput)) {
+            const [title, description, people] = userInput;
+            console.log(title, description, people);
+        }
+    }
+    
+    private configure() {
+        this.element.addEventListener('submit', this.submitHandler);
+    }
+
+    private attach() {
+        this.hostElement.insertAdjacentElement('afterbegin', this.element);
+    }
+}
+
+const prjInput = new ProjectInput();
