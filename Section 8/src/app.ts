5267e0883f6fb23fba124ef256d45d23476565ba
Lirone Fitoussi <95374011+LironeFitoussi@users.noreply.github.com>
2024-04-26 09:00:33 +0300

    Progress-Lesson

--- Section 8/src/app.ts
+++ Section 8/src/app.ts
@@ -31,3 +31,48 @@ class Person {
 
 const person = new Person();
 console.log(person);
+
+function Log(target: any, propertyName: string | Symbol) {
+    console.log('Property decorator!');
+    console.log(target, propertyName);
+}
+
+function Log2(target: any, name: string, descriptor: PropertyDescriptor) {
+    console.log('Accessor decorator!');
+    console.log(target);
+    console.log(name);
+    console.log(descriptor);
+}
+
+function Log3(target: any, name: string | Symbol, descriptor: PropertyDescriptor) {
+    console.log('Method decorator!');
+    console.log(target);
+    console.log(name);
+    console.log(descriptor);
+}
+
+
+class Product {
+    @Log
+    title: string;
+    private _price: number;
+
+    @Log2
+    set price(val: number) {
+        if (val > 0) {
+            this._price = val;
+        } else {
+            throw new Error('Invalid price - should be positive!');
+        }
+    }
+
+    constructor(t: string, p: number) {
+        this.title = t;
+        this._price = p;
+    }
+
+    @Log3
+    getPriceWithTax(tax: number) {
+        return this._price * (1 + tax);
+    }
+}
