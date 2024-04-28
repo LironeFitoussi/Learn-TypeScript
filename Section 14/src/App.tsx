9888025e52b992d32b133998408c2efcc950c09a
LironeFitoussi <95374011+LILo2610@users.noreply.github.com>
2024-04-28 12:34:30 +0300

    dd

--- Section 14/src/App.tsx
+++ Section 14/src/App.tsx
@@ -1,8 +1,19 @@
 import React from 'react'
+
+import TodoList from './components/TodoList'
+
+
 const App: React.FC = () => {
+  const todos = [
+      { id: 't1', text: "Learn React" },
+      { id: 't2', text: "Learn TypeScript" },
+  ];
+  
   return (
     <div className="pp">
-
+      <h1>My Todos</h1>
+      {/* A component that add todos */}
+      <TodoList items={todos}/>
     </div>
   )
 }
