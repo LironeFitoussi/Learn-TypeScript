1645db634caecc2f8f4bf8643b5df8ef304b4b1e
LironeFitoussi <95374011+LILo2610@users.noreply.github.com>
2024-04-28 12:03:01 +0300

    Done

--- Section 13/src/app.ts
+++ Section 13/src/app.ts
@@ -1,11 +1,50 @@
+import axios from 'axios';
 const form = document.querySelector('form')!;
 const addressInput = document.getElementById('address')! as HTMLInputElement;
+
+const GOOGLE_API_KEY = 'AIzaSyD_gPyTxCYlWehgKy6ZTRkEemH1iyFumVs'
+
+// declare var google: any; //
+
+type GoogleGeocodingResponse = {
+    results: { geometry: { location: { lat: number, lng: number } } }[];
+    status: 'OK' | 'ZERO_RESULTS';
+}
 
 function searchAddressHandler(e: Event) {
     e.preventDefault();
     const enteredAddress = addressInput.value;
-    console.log(enteredAddress);
+    // console.log(enteredAddress);
+    axios.get<GoogleGeocodingResponse>(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(enteredAddress)}&key=${GOOGLE_API_KEY}`)
+        .then(response => {
+            if (response.data.status !== 'OK') {
+                throw new Error('Could not fetch location!');
+            }
+            
+            const coordinates = response.data.results[0].geometry.location;
+            console.log(coordinates);
+            // const map = new google.maps.Map(document.getElementById('map')!, {
+            //     center: coordinates,
+            //     zoom: 16
+            // });
 
+            // new google.maps.Marker({ position: coordinates, map: map });
+
+            let map: google.maps.Map;
+            async function initMap(): Promise<void> {
+                const { Map } = await google.maps.importLibrary("maps") as google.maps.MapsLibrary;
+                map = new Map(document.getElementById("map") as HTMLElement, {
+                    center: coordinates,
+                    zoom: 16,
+                });
+                new google.maps.Marker({position: coordinates, map});
+            }
+
+            initMap();
+        })
+        .catch(err => {
+            console.log(err);
+        });
     // send this to Google's API
 }
 
